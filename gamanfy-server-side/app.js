5b7626bd7d99c9531c64021d9dbfb3eb3b87e833
unai gonzalez <unaigoe91@gmail.com>
2020-06-04 12:32:09 +0200

    cors

--- gamanfy-server-side/app.js
+++ gamanfy-server-side/app.js
@@ -51,13 +51,13 @@ app.use(
 );
 
 // CORS MIDDLEWARE SETUP
-/* app.use(
+app.use(
   cors({
     credentials: true,
     origin: [process.env.PUBLIC_DOMAIN],
   })
-); */
-let allowCrossDomain = function(req, res, next) {
+);
+/* let allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', [process.env.PUBLIC_DOMAIN]);
   res.header('Access-Control-Allow-Methods', 'GET,PUT, OPTIONS, POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -65,7 +65,7 @@ let allowCrossDomain = function(req, res, next) {
 
   next();
 }
-app.use(allowCrossDomain);
+app.use(allowCrossDomain); */
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
