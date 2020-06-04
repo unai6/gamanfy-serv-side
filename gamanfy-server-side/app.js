a357ec0943b29eb0822edfa509addbb2c4002435
unai gonzalez <unaigoe91@gmail.com>
2020-06-04 16:41:42 +0200

    cors

--- gamanfy-server-side/app.js
+++ gamanfy-server-side/app.js
@@ -55,11 +55,11 @@ let allowedOrigins = [process.env.PUBLIC_DOMAIN,
                       process.env.APP_DOMAIN];
 app.use(
   cors({
-    credentials: true,
+    credentials: true,  
     methods:'GET, POST, PUT, DELETE, OPTIONS, HEAD',
     origin: function(origin, callback){
       if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
+      if(allowedOrigins.indexOf(origin) !== -1){
         var msg = 'The CORS policy for this site does not ' +
                   'allow access from the specified Origin.';
         return callback(new Error(msg), false);
