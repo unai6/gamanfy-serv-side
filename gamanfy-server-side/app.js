d58c94b047eed8cc0d5efb5bcef8156fdc3ad9cd
unai gonzalez <unaigoe91@gmail.com>
2020-06-04 16:22:22 +0200

    cors

--- gamanfy-server-side/app.js
+++ gamanfy-server-side/app.js
@@ -68,7 +68,7 @@ app.use(
 }
 app.use(allowCrossDomain) */;
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", [process.env.PUBLIC_DOMAIN]  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
