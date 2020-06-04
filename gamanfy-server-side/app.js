41f0c72475335652a395cbdd4cbfd604d1023522
unai gonzalez <unaigoe91@gmail.com>
2020-06-04 13:45:35 +0200

    cors

--- gamanfy-server-side/app.js
+++ gamanfy-server-side/app.js
@@ -23,70 +23,80 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 
-// MONGOOSE CONNECTION
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useUnifiedTopology: true,
-    keepAlive: true,
-    useNewUrlParser: true,
-    useFindAndModify: false 
-  })
-  .then(() => console.log(`Connected to database`))
-  .catch((err) => console.error(err));
-
-  // SESSION MIDDLEWARE
-app.use(
-  session({
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      ttl: 24 * 60 * 60, // 1 day
-    }),
-    secret: process.env.SECRET_SESSION,
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 24 * 60 * 60 * 1000,
-    },
-  })
-);
-
 // CORS MIDDLEWARE SETUP
 app.use(
   cors({
     credentials: true,
     origin: [process.env.PUBLIC_DOMAIN],
-  })
-); 
-
+  })  
+  );   
+  
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', process.env.PUBLIC_DOMAIN);
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
-});
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+});  
 
-app.use(logger('dev'));
+// MONGOOSE CONNECTION
+mongoose
+.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  keepAlive: true,
+  useNewUrlParser: true,
+  useFindAndModify: false 
+})  
+.then(() => console.log(`Connected to database`))
+.catch((err) => console.error(err));
+
+// SESSION MIDDLEWARE
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day
+    }),  
+    secret: process.env.SECRET_SESSION,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },  
+  })  
+  );  
+  
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'hbs');
+  
+  app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', indexRouter);
 app.use('/auth', userAuthRouter);
 app.use('/auth-co', companyAuthRouter);
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error("ERROR", req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    const statusError = err.status || "500";
+    res.status(statusError).json(err);
+  }
+});
 
 
 // error handler
