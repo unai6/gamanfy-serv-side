60a3610cf2cfa4a5b3d910826ec5d45bc88d6d3a
unai gonzalez <unaigoe91@gmail.com>
2020-06-05 11:42:41 +0200

    job offer model

--- gamanfy-server-side/models/JobOffer.js
+++ gamanfy-server-side/models/JobOffer.js
@@ -3,6 +3,29 @@ const Schema = mongoose.Schema;
 
 const jobOfferSchema = new Schema  ({
 
+      scorePerRec : {type: String, default:'5'},
+      moneyPerRec:String,
+      contractServices : [{
+            type:{type:String},
+            sourcingWithInfluencer : {type:Boolean, default: false},
+            exclusiveHeadHunter : {type:Boolean, default:false}
+      }],
+      additionalServices :[{
+            type: {type:String},
+            personalityTest:{type:Boolean, default: false},
+            videoInterview : {type: Boolean, default: false},
+            kitOnBoardgingGamanfy: {type: Boolean, default: false}
+      }],
+      gamanfyFee:[{
+            type:{type:String},
+            sourcingWithInfluencer:{type:String},
+            exclusiveHeadHunter:{type:String},
+            personalityTest:{type:String},
+            videoInterview: {type:String},
+            kitOnBoardgingGamanfy:{type:String},
+            totalFee:{type:String}
+      }],
+
       companyId : String,
       charge: String,
       description: String,
