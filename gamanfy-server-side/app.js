be232077f47fba3f0e29ff535f50de515039bc90
unai gonzalez <unaigoe91@gmail.com>
2020-06-08 13:02:14 +0200

    cookies

--- gamanfy-server-side/app.js
+++ gamanfy-server-side/app.js
@@ -48,7 +48,8 @@ mongoose
       saveUninitialized: true,
       cookie: {
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite:"none"
+        sameSite:"none",
+        secure:true
       },
     })
     );
