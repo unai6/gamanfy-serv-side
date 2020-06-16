88b801a527dac8fec6af6236ad3bbdf70eacb8f8
unai gonzalez <unaigoe91@gmail.com>
2020-06-16 15:13:34 +0200

    erased useless fields

--- gamanfy-server-side/routes/offers/offers.js
+++ gamanfy-server-side/routes/offers/offers.js
@@ -16,7 +16,7 @@ const Address = require('../../models/Address');
 router.get('/dashboard', async (req, res, next) => {
 
     try {
-        let allOffers = await Offers.find({"contractServices.hasSourcingWithInfluencer" : true}).limit(10);
+        let allOffers = await Offers.find({ "contractServices.hasSourcingWithInfluencer": true }).limit(10);
         allOffers.length !== 0
             ? res.json({ allOffers })
             : res.status(404).json({ message: "No products found!" });
@@ -28,15 +28,16 @@ router.get('/dashboard', async (req, res, next) => {
 
 router.get('/getData/:companyId', async (req, res) => {
 
-    try{ const {companyId} = req.params;
+    try {
+        const { companyId } = req.params;
 
-    let getCompanyData = await Company.findById(companyId).populate("postedOffers")
-    res.status(200).json(getCompanyData);
-        
-    }catch(error){
-        res.status(400).json({mssg: 'error'})
+        let getCompanyData = await Company.findById(companyId).populate("postedOffers")
+        res.status(200).json(getCompanyData);
+
+    } catch (error) {
+        res.status(400).json({ mssg: 'error' })
     }
-   
+
 })
 
 router.post('/:companyId/post-job-offer', async (req, res, next) => {
@@ -45,47 +46,35 @@ router.post('/:companyId/post-job-offer', async (req, res, next) => {
         const { companyId } = req.params;
         //scorepunc
         const { scorePerRec, moneyPerRec } = req.body;
-
         //contract services
         const { hasSourcingWithInfluencer, hasExclusiveHeadHunter } = req.body;
-
         //additional services
         const { hasPersonalityTest, hasVideoInterview, hasKitOnBoardingGamanfy } = req.body;
-
         //gamanfy fee plus additional services
-        const { totalFee, sourcingWithInfluencer, exclusiveHeadHunter, personalityTest, videoInterview, kitOnBoardgingGamanfy } = req.body;
-
+        const { totalFee } = req.body;
         //company data 
         let { processNum, description, website, recruiter } = req.body;
-        processNum = crypto.randomBytes(2).toString('hex')    
+        processNum = crypto.randomBytes(2).toString('hex')
         //job offer data
         const { jobName, onDate, offDate, processState, isRemote, personsOnCharge, team } = req.body;
         //job description
         const { mainMission, jobDescription } = req.body;
         //manager
-        const { managerDescription, managerName } = req.body;   
-
+        const { managerDescription, managerName } = req.body;
         //job address
         const { countryCode, countryName, provinceINEcode, municipalityINEcode, street, number, zip, cityForOffer } = req.body;
-
-        //category
-        const { employee, specialist, intermediateResp, direction, directiveCouncil, cofounder } = req.body;
-
-        //contract
-        const { autonomo, contratoDeDuraciónDeterminada, deRelevo, fijoDiscontinuo, formativo, Indefinido, aTiempoParcial, otrosContratos } = req.body;
-
         //retribution
         const { minGrossSalary, maxGrossSalary, variableRetribution, quantityVariableRetribution, showMoney } = req.body;
         // min requirements
-        const { minExp, minStudies, keyKnowledge, keyComp, minReqDescription, Language, LangugageLevel } = req.body;
+        const { minExp, minStudies, keyKnowledge, keyComp, minReqDescription, language, langugageLevel } = req.body;
         //interview Questions
         const { question1, question2, question3, question4, question5 } = req.body;
 
 
-        let company = await Company.findById(companyId) ;
-        
+        let company = await Company.findById(companyId);
 
-        if(company.description !== '' || null){
+
+        if (company.description !== '' || null) {
             description = company.description
         };
 
@@ -98,18 +87,17 @@ router.post('/:companyId/post-job-offer', async (req, res, next) => {
             moneyPerRec,
             contractServices: { hasSourcingWithInfluencer, hasExclusiveHeadHunter },
             additionalServices: { hasPersonalityTest, hasVideoInterview, hasKitOnBoardingGamanfy },
-            gamanfyFee: { totalFee},
+            gamanfyFee: { totalFee },
             companyData: { processNum, description, website, recruiter },
             jobOfferData: {
-                jobName, onDate, offDate, processState, isRemote, personsOnCharge,
-                
+                jobName, onDate, offDate, processState, isRemote, personsOnCharge
             },
-            jobDescription : {mainMission, team, jobDescription},
-            manager:{managerDescription, managerName},
+            jobDescription: { mainMission, team, jobDescription },
+            manager: { managerDescription, managerName },
             addressId, sectorId, categoryId, contractId,
             retribution: { minGrossSalary, maxGrossSalary, variableRetribution, quantityVariableRetribution, showMoney },
-            minRequirements: { minExp, minStudies, keyKnowledge, minReqDescription, Language, LangugageLevel },
-            keyCompetences:[keyComp],
+            minRequirements: { minExp, minStudies, keyKnowledge, minReqDescription, language, langugageLevel },
+            keyCompetences: { keyComp },
             videoInterviewQuestions: { question1, question2, question3, question4, question5 }
         });
         let updatedCompany = await Company.findByIdAndUpdate(company, { $push: { postedOffers: postedOffers._id } }, { new: true })
@@ -138,7 +126,7 @@ router.put('/:companyId/:offerId/edit-offer', async (req, res) => {
         const { hasPersonalityTest, hasVideoInterview, hasKitOnBoardingGamanfy } = req.body;
 
         //gamanfy fee plus additional services
-        const { totalFee} = req.body;
+        const { totalFee } = req.body;
 
         //company data 
         const { processNum, description, website, recruiter } = req.body;
@@ -153,25 +141,19 @@ router.put('/:companyId/:offerId/edit-offer', async (req, res) => {
         //job address
         const { countryCode, countryName, provinceINEcode, municipalityINEcode, street, number, zip, cityForOffer } = req.body;
 
-        //category
-        const { employee, specialist, intermediateResp, direction, directiveCouncil, cofounder } = req.body;
-
-        //contract
-        const { autonomo, contratoDeDuraciónDeterminada, deRelevo, fijoDiscontinuo, formativo, Indefinido, aTiempoParcial, otrosContratos } = req.body;
-
         //retribution
         const { minGrossSalary, maxGrossSalary, variableRetribution, quantityVariableRetribution, showMoney } = req.body;
         // min requirements
-        const { minExp, minStudies, keyKnowledge, keyComp, minReqDescription, Language, LangugageLevel } = req.body;
+        const { minExp, minStudies, keyKnowledge, keyComp, minReqDescription, language, langugageLevel } = req.body;
         //interview Questions
         const { question1, question2, question3, question4, question5 } = req.body;
 
         let myCompany = await Company.findById(companyId);
         let offerInDB = await Offers.findById(offerId);
-        let addressId = await Address.findByIdAndUpdate(myCompany.addressId, { countryCode, countryName, provinceINEcode, municipalityINEcode, street, number, zip, cityForOffer }, {new:true});
-        let sectorId = await Sector.findByIdAndUpdate(myCompany.sectorId, {sector}, {new:true});
-        let categoryId = await Category.findByIdAndUpdate(myCompany.categoryId, {category}, {new:true});
-        let contractId = await Contract.findByIdAndUpdate(myCompany.contractId, {contract}, {new:true});
+        let addressId = await Address.findByIdAndUpdate(myCompany.addressId, { countryCode, countryName, provinceINEcode, municipalityINEcode, street, number, zip, cityForOffer }, { new: true });
+        let sectorId = await Sector.findByIdAndUpdate(myCompany.sectorId, { sector }, { new: true });
+        let categoryId = await Category.findByIdAndUpdate(myCompany.categoryId, { category }, { new: true });
+        let contractId = await Contract.findByIdAndUpdate(myCompany.contractId, { contract }, { new: true });
 
         let updatedOffer = await Offers.findByIdAndUpdate(offerInDB._id, {
             $set: {
@@ -180,14 +162,13 @@ router.put('/:companyId/:offerId/edit-offer', async (req, res) => {
                 gamanfyFee: { totalFee },
                 companyData: { processNum, description, website, recruiter },
                 jobOfferData: {
-                    jobName, onDate, offDate, processState, isRemote, personsOnCharge,
-                    
+                    jobName, onDate, offDate, processState, isRemote, personsOnCharge
                 },
-                jobDescription : {mainMission, team, jobDescription},
-                manager:{managerDescription, managerName},
+                jobDescription: { mainMission, team, jobDescription },
+                manager: { managerDescription, managerName },
                 retribution: { minGrossSalary, maxGrossSalary, variableRetribution, quantityVariableRetribution, showMoney },
-                minRequirements: { minExp, minStudies, keyKnowledge, minReqDescription, Language, LangugageLevel },
-                keyCompetences :{keyComp},
+                minRequirements: { minExp, minStudies, keyKnowledge, minReqDescription, language, langugageLevel },
+                keyCompetences: { keyComp },
                 videoInterviewQuestions: { question1, question2, question3, question4, question5 },
                 scorePerRec, moneyPerRec, addressId, sectorId, categoryId, contractId
             },
@@ -206,13 +187,13 @@ router.put('/:companyId/:offerId/edit-offer', async (req, res) => {
 router.post('/:companyId/:offerId/delete-offer', async (req, res) => {
     try {
         const { companyId, offerId } = req.params;
-        let updatedCompany = await Company.findByIdAndUpdate(companyId, { $pull : { postedOffers : { $in : [ offerId ] } } }, { multi:true });
+        let updatedCompany = await Company.findByIdAndUpdate(companyId, { $pull: { postedOffers: { $in: [offerId] } } }, { multi: true });
         await Offers.findByIdAndRemove(offerId)
-        
-        res.status(200).json({message:'offer deleted succesfully'});
-    
+
+        res.status(200).json({ message: 'offer deleted succesfully' });
+
     } catch (error) {
-        res.status(400).json({message: 'An error occurred while trying to delete the offer'});
+        res.status(400).json({ message: 'An error occurred while trying to delete the offer' });
     }
 
 
