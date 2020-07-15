7a75804c85239139e7cc5ccffa9d42b6957f0eb9
unai gonzalez <unaigoe91@gmail.com>
2020-07-15 10:54:42 +0200

    email styles added

--- gamanfy-server-side/appControllers/userControllers/userAuthController.js
+++ gamanfy-server-side/appControllers/userControllers/userAuthController.js
@@ -191,16 +191,16 @@ exports.userSignup = async (req, res, next) => {
         to: newUser.email,
         subject: 'Verificación de la Cuenta',
         html: `
-          <img style='height:6em' <img src="cid:unique@nodemailer.com"/>
-          <div>
-          <p style='font-weight:600; color:#535353; font-size:18px; margin-left:1em'> ¡Hola ${newUser.firstName}! Nos alegramos mucho<br> de poder contar contigo </p>\n
-      
-            <div style='font-weight:300; color:#535353; font-size:14px; margin-top:1.5em'>
-              Tu cuenta ha sido creada y ya tienes todo listo para comenzar. </br>
-              Haz click en este botón para verificar tu cuenta.</br>
+        <img style='height:6em' <img src="cid:unique@nodemailer.com"/>
+        <div>
+            <p style='font-weight:600; color:#535353; font-size:18px; margin-left:1em'> ¡Hola ${newUser.firstName}! Nos alegramos mucho<br> de poder contar contigo </p>\n
+
+             <div style='font-weight:300; color:#535353; font-size:14px; margin:1.5em 0 1em 1em'>
+                 Tu cuenta ha sido creada y ya tienes todo listo para comenzar. </br>
+                 Haz click en este botón para verificar tu cuenta.</br>
             </div>
-              <button type='submit' style='border:none; background:rgb(255,188,73); border-radius:5px; width:14em; height:2.5em; margin-top:2em; margin-left:11em'><a href="${process.env.PUBLIC_DOMAIN}/auth/confirmation/${newUser._id}/${token.token}/${newUser.isCompany}" style="color:white; text-decoration:none; font-weight:500">Verificar cuenta</a></button><br/>
-          </div> \n`,
+            <a href="${process.env.PUBLIC_DOMAIN}/auth/confirmation/${newUser._id}/${token.token}/${newUser.isCompany}" style="color:white; text-decoration:none; border:none !important; background-color:rgb(255,188,73); border-radius:5px; width:14em; padding:.2em .5em .2em .5em; height:2.5em; margin-top:2em; margin-left:11em; font-weight:500">Verificar cuenta</a><br/>
+        </div> \n`,
         attachments: [{
           filename: 'logo-gamanfy-email.png',
           path: 'public/logo-gamanfy-email.png',
