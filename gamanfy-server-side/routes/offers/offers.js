c6d054562e2cbb7770ae74934a3b981c173d4cd9
unai gonzalez <unaigoe91@gmail.com>
2020-07-30 19:03:02 +0200

    routes for report and candidates info added

--- gamanfy-server-side/routes/offers/offers.js
+++ gamanfy-server-side/routes/offers/offers.js
@@ -46,11 +46,11 @@ router.get('/offer-details/:offerId', async (req, res) => {
     }
 })
 
-router.get('/candidates/:offerId', async (req, res) => {
+router.get('/candidates/:offerId/:companyId', async (req, res) => {
     try {
 
-        let { offerId } = req.params;
-         await Offers.findById(offerId).populate({
+        let { offerId, companyId } = req.params;
+        await Offers.findById(offerId).populate({
 
             path: 'jobOffers.recommendedTimes',
             model: 'JobOffer'
@@ -64,7 +64,8 @@ router.get('/candidates/:offerId', async (req, res) => {
             }
         })
 
-
+         await Company.findById(companyId)
+      
     } catch (error) {
         res.status(400).json({ error: 'Error' })
     }
@@ -287,32 +288,28 @@ router.post('/:companyId/:offerId/delete-offer', async (req, res) => {
 
 })
 
-router.post('/company/infoRequest/:offerId', async (req, res) => {
+router.post('/company/infoRequest/:offerId/:companyId/:recommendationId', async (req, res) => {
     try {
-     
-        let { offerId } = req.params;
-         await Offers.findById(offerId).populate({
 
-            path: 'jobOffers.recommendedTimes',
-            poulate:{
-                path:'recommendedTimes.offerId',
-                populate:{
-                    path:'offerId',
-                    model:'JobOffer'
+        let { offerId, companyId, recommendationId } = req.params;
+        let recommendToPopulate = await Offers.findById(offerId).populate({
+
+            path: 'recommendedTimes.offerId',
+            poulate: {
+                path: 'offerId',
+                populate: {
+                    path: 'offerId',
+                    model: 'JobOffer'
                 }
-            }
-            
+            }   
+        });
 
-        }).exec(function (err, recommendedTimes) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.status(200).json(recommendedTimes.recommendedTimes);
-                console.log("success");
-            }
-        })
 
-        /* let transporter = nodemailer.createTransport({
+        let recommendation = await Recommended.findById(recommendationId)
+
+        let company = await Company.findById(companyId)
+
+        let transporter = nodemailer.createTransport({
 
             host: 'smtp.ionos.es',
             port: 587,
@@ -333,22 +330,21 @@ router.post('/company/infoRequest/:offerId', async (req, res) => {
 
         let mailOptions = {
             from: process.env.HOST_MAIL,
-            to: recommendedEmail,
-            subject: 'Gamanfy, ¡Te damos la bienvenida!',
+            to: company.email,
+            subject: 'Gamanfy, Informe de candidato',
             html: `
            <img style='height:6em' <img src="cid:unique@nodemailer.com"/>
             <div>
-            <p style='font-weight:600; color:#535353; font-size:18px; margin-left:1em'> ¡Hola ${recommendedFirstName}! <b>${influencerUserName}</b>  te ha recomendado <br/> para una oferta de trabajo en Gamanfy. </p>\n
+            <p style='font-weight:600; color:#535353; font-size:18px; margin-left:1em'> ¡Hola ${company.companyName}!, como se ha requerido, <br/> te adjuntamos el informe del candidato. </p>\n
+            <p style='font-weight:600; color:#535353; font-size:18px; margin-left:1em'>Saludos,</p>\n
+            <p style='font-weight:600; color:#535353; font-size:18px; margin-left:1em'> Gamanfy Staff.</p>\n
             <div style='font-weight:300; color:#535353; font-size:14px'>
-      
-  
-              </div>
+    
+            </div>
               <div style='font-weight:300; color:#535353; font-size:14px; margin-top:1.5em'>
-              Si quieres ver la oferta completa  y enterarte de todo lo que Gamanfy </br>puede ofrecerte, haz click en <b><u><a href='${process.env.PUBLIC_DOMAIN}/auth/user/signup' style='color:#535353; text-decoration:none'>Regístrarte</a> </u></b><br/>
-              <button type='submit' style="border:none; background:rgb(255,188,73); border-radius:5px; width:14em; height:2.5em; margin-top:2em; margin-left:11em"><a href='${process.env.PUBLIC_DOMAIN}/offer-details/${theOffer._id}' style='color:white; text-decoration:none; font-weight:500'>Ver oferta completa</a></button><br/>
+              <button type='submit' style="border:none; background:rgb(255,188,73); border-radius:5px; width:14em; height:2.5em; margin-top:2em; margin-left:11em"><a href=${process.env.PUBLIC_DOMAIN}/${recommendationId}/candidate-info style='color:white; text-decoration:none; font-weight:500'>Ver informe del candidato</a></button><br/>
               </div>
           
-        
           </div>
           `,
             attachments: [{
@@ -360,10 +356,11 @@ router.post('/company/infoRequest/:offerId', async (req, res) => {
 
         transporter.sendMail(mailOptions, function (err) {
             if (err) { return res.status(500).send({ msg: err.message }); } else {
-            res.status(200).send('A verification recommendedEmail has been sent to ' + recommendedEmail + '.');
+                res.status(200)
             }
         });
- */
+
+        res.status(200).json({ 'recommendation': recommendation, 'company': company, 'populatedRec': recommendToPopulate })
 
     } catch (error) {
         res.status(400).json({ error: 'Error' })
@@ -371,5 +368,20 @@ router.post('/company/infoRequest/:offerId', async (req, res) => {
 
 });
 
+router.post('/:recommendationId/candidate-info', async (req, res) => {
+
+    try {
+
+        let { recommendationId } = req.params;
+
+        let recommendation = await Recommended.findById(recommendationId);
+
+        res.status(200).json(recommendation)
+    } catch (error) {
+        res.status(400).json({ error: 'Error' })
+    }
+
+
+})
 
 module.exports = router
