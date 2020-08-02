c13d51989ee7f8f24771936c371d52b97bdbb911
unai gonzalez <unaigoe91@gmail.com>
2020-08-02 20:56:06 +0200

    recommed route updated

--- gamanfy-server-side/appControllers/companyControllers/recommend.js
+++ gamanfy-server-side/appControllers/companyControllers/recommend.js
@@ -5,10 +5,15 @@ const Recommended = require('../../models/Recommended');
 exports.recommend = async (req, res, next) => {
 
     try {
-        const { recommendedEmail, recommendedFirstName, recommendedLastName, whyRec } = req.body;
+        const { recommendedEmail, recommendedFirstName, curriculum, recommendedLinkedin, recommendedPhoneNumber, recommendedLastName, whyRec, 
+            specificEducation, howMet, sectorBestFit, departmentBestFit, competences, language, candidateLocation, otherAspects } = req.body;
         const { companyId } = req.params;
         await Company.findById(companyId);
-        let recommendedProfessionals = await Recommended.create({ recommendedEmail, recommendedFirstName, recommendedLastName, whyRec  })
+        let recommendedProfessionals = await Recommended.create({ recommendedEmail, recommendedFirstName, curriculum, recommendedLinkedin, 
+            recommendedPhoneNumber, recommendedLastName, whyRec,
+            candidateInfo:{
+            specificEducation, howMet, sectorBestFit, departmentBestFit, competences, language, candidateLocation, otherAspects} 
+        });
 
         const updatedUser = await Company.findByIdAndUpdate(companyId, { $push: { recommendedProfessionals: recommendedProfessionals._id } }, { new: true })
         res.status(200).json({ updatedUser })
