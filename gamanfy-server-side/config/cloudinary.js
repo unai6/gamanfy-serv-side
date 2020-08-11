3d6258001b581b0247174fc048aef50a35f389d1
unai gonzalez <unaigoe91@gmail.com>
2020-08-11 18:35:56 +0200

    nothing

--- gamanfy-server-side/config/cloudinary.js
+++ gamanfy-server-side/config/cloudinary.js
@@ -1,5 +1,5 @@
 const multer = require('multer');
- const cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
@@ -11,10 +11,9 @@ cloudinary.config({
 let storage =  new CloudinaryStorage({
   cloudinary,
   folder: 'gamanfy', // The name of the folder in cloudinary
-  params: { resource_type: 'raw' },
-  filename: function (req, res, cb) {
-    let fileName = res.originalname.split(".");
-    cb(null, fileName[0]); // The file on cloudinary would have the same name as the original file name
+  allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'mp3', 'wav', 'pdf'],
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
   }
 });
 
