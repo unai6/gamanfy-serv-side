8d28c9aab9c8eddfdbaf9787dc9d651f11b48fdb
unai gonzalez <unaigoe91@gmail.com>
2020-08-24 12:40:43 +0200

    restart of heroku server

--- gamanfy-server-side/config/companyPicsUploader.js
+++ gamanfy-server-side/config/companyPicsUploader.js
@@ -1,32 +1,4 @@
 const multer = require('multer');
-/*
-const { uuid } = require('uuidv4');
-
-
-const DIR = './public/companyPictures';
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, DIR);
-  },
-  filename: (req, file, cb) => {
-      const fileName = file.originalname.toLowerCase().split(' ').join('-');
-      cb(null, uuid() + '-' + fileName)
-  }
-});
-
-let companyPicUploader = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-      if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-          cb(null, true);
-      } else {
-          cb(null, false);
-          return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-      }
-  }
-});
- */
 
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
@@ -56,3 +28,32 @@ module.exports = companyPicUploader
 
 
 
+
+/*
+const { uuid } = require('uuidv4');
+
+
+const DIR = './public/companyPictures';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, DIR);
+  },
+  filename: (req, file, cb) => {
+      const fileName = file.originalname.toLowerCase().split(' ').join('-');
+      cb(null, uuid() + '-' + fileName)
+  }
+});
+
+let companyPicUploader = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+      if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+          cb(null, true);
+      } else {
+          cb(null, false);
+          return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+      }
+  }
+});
+ */
