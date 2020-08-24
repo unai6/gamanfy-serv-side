ce973c6ea9a45edf01386312fc573ab7b9fb42e4
unai gonzalez <unaigoe91@gmail.com>
2020-08-24 17:04:37 +0200

    viewa out node

--- gamanfy-server-side/app.js
+++ gamanfy-server-side/app.js
@@ -44,6 +44,10 @@ mongoose
       );  
 
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
