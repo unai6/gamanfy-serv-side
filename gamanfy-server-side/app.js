3c23b96478e696a5ad948bf593b1c8286d45d2a4
unai gonzalez <unaigoe91@gmail.com>
2020-11-23 13:30:43 +0100

    cors

--- gamanfy-server-side/app.js
+++ gamanfy-server-side/app.js
@@ -40,6 +40,7 @@ mongoose
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", ["http://localhost:3000", "https://gamanfy-c2371.web.app", "http://www.fontawesome.com", 'https://app.gamanfy.com', process.env.PUBLIC_DOMAIN]);
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Credentials", true);
       next();
     });
 
