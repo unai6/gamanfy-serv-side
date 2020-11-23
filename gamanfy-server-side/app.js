b4db8d2bbb4537a6b9c558837a5f05e5e8547fab
unai gonzalez <unaigoe91@gmail.com>
2020-11-23 14:08:26 +0100

    cors

--- gamanfy-server-side/app.js
+++ gamanfy-server-side/app.js
@@ -40,7 +40,7 @@ mongoose
 app.use(
   cors({
     credentials: true,
-    origin:'*'
+    origin:["http://localhost:3000", "https://gamanfy-c2371.web.app", "http://www.fontawesome.com", "https://app.gamanfy.com"]
   })
   );  
 
